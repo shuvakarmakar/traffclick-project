d835afb44a80c7bafc5941a98c32af55ec591e69
shuvakarmakar <shuva15-7953@diu.edu.bd>
2024-07-26 15:03:17 +0600

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 color='Gray'>Hello WOrld and React Js I am back</h1>
+            <h3 className='font-bold text-3xl'>My Name is Shuva Karmakar</h3>
+        </div>
+    );
+};
+
+export default Home;
