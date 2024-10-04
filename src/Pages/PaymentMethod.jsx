8b1a0b804a89da80307f59b908b8c7800fc5ff67
shuvakarmakar <shuva15-7953@diu.edu.bd>
2024-10-04 13:42:14 +0600

    PaymentMethod Component Added"

---
+++ src/Pages/PaymentMethod.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PaymentMethod = () => {
+    return (
+        <div>
+            <h2>This is For Payment Method</h2>
+        </div>
+    );
+};
+
+export default PaymentMethod;
